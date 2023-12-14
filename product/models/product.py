a1d76695ff43f5660198a962f47cd6ee04baf027
romario lima <romariolima1006@gmail.com>
2023-12-14 11:19:38 -0300

    commit de correcao

--- product/models/product.py
+++ product/models/product.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 from django.db import models
 
 from product.models import Category
@@ -8,4 +12,8 @@ class Product(models.Model):
     description = models.TextField(max_length=500, blank=True, null=True)
     price = models.PositiveIntegerField(null=True)
     active = models.BooleanField(default=True)
-    categories = models.ManyToManyField(Category, blank=True)
+    category = models.ManyToManyField(Category, blank=True)
+
+
+    def __str__(self):
+        return self.title
